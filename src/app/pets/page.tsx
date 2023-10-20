fe4b0378a339164fb296060cbf7f150921f08b16
wllysses <wllyssestavares@gmail.com>
2023-10-20 14:36:29 -0300

    fix: spinner loading on pets page

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -14,6 +14,8 @@ export default function Pets() {
 
     return (
         <>
+            { isLoading && <Spinner /> }
+            { isError && <span>Algo deu errado 😢</span> }
             <Header />
             <main className="container mx-auto my-12 p-4">
                 <section className="w-full px-4 py-8 bg-secondary-color rounded flex items-center justify-center flex-col gap-2 text-white">
@@ -23,8 +25,6 @@ export default function Pets() {
                 <section className="w-full mt-16">
                     <h3 className="text-center font-bold text-primary-color text-3xl">Nossos Pets</h3>
                     <div className="w-full mt-12 min-h-[60vh] grid grid-cols-5 gap-8 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2">
-                        { isLoading && <Spinner /> }
-                        { isError && <span>Algo de errado aconteceu 😢</span> }
                         {pets && pets.map((pet) => (
                             <Card key={pet.id} data={pet} />
                         ))}
